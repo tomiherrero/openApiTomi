92eba098ec319e48e61b744bce0f185f28cd4ef8
tomiherrero <tomas.herrero@hotmail.es>
2020-04-20 23:33:53 -0300

    Modified error

--- src/openapi/api/cars.js
+++ src/openapi/api/cars.js
@@ -38,37 +38,45 @@ module.exports = {
                     content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
                 }
             }
-        },
-        '/api/cars/{id}': {
-            put: {
-                security: [],
-                parameters: [
-                    {
-                        in: 'path',
-                        name: 'id',
-                        schema: {
-                            type: 'string',
-                            format: 'uuid'
-                        },
-                        required: true,
-                        description: 'Id del auto'
-                    }
-                ],
-                requestBody: {
-                    description: 'Optional description in Markdown',
-                    content: {'application/json': {schema: {$ref: '#/components/schemas/Car'}}}
-                },
-                responses: {
-                    200: {
-                        description: 'list of Cars',
-                        content: {'application/json': {schema: {$ref: '#/components/schemas/Car'}}}
+        }
+    },
+    '/api/cars/{id}': {     
+        put: {
+            security: [],
+            parameters: [
+                {
+                    in: 'path',
+                    name: 'brand',
+                    schema: {
+                        type: 'string',
+                        format: 'uuid'
                     },
-                    default: {
-                        description: 'Error',
-                        content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                    required: true,
+                    description: 'Marca del auto solicitada'
+                }
+            ],
+            requestBody: {
+                description: 'Optional description in Markdown',
+                required: true,
+                content: {'application/json': {schema: {$ref: '#/components/schemas/Car'}}}
+            },
+            responses: {
+                200: {
+                    description: 'list of Cars',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+                            }
+                        }
                     }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
                 }
             }
-        }    
+        },
     }
 }
